db6cf75cccebc0a3b581e5bbee021702302b8f4a
bruno oliveira <brunooliveira.2115b@gmail.com>
2023-10-26 20:39:17 -0300

    Iniciado Atualizar

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 import { app, db } from "./config-firebase.js"
-import { doc, setDoc, collection, addDoc, query, where, getDocs, orderBy} from "https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js"
+import { doc, setDoc, collection, addDoc, query, where, getDocs, orderBy, deleteDoc} from "https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js"
 
 let nome = document.querySelector("#tarefa")
 let data = document.querySelector("#data")
 let status = document.querySelector("#status")
 let btnTarefa = document.querySelector("#btnTarefa")
 let bloco = document.querySelector("#bloco")
+let formCadastrar = document.querySelector("#formCadastrar")
+let formAtualizar = document.querySelector("#formAtualizar")
+let btnAtualizar = document.querySelector("#btnAtualizar")
 
 async function inserirTarefa(){
     try {
@@ -24,6 +27,7 @@ const docRef = await addDoc(collection(db, "tarefa"), {
     
 }
 async function consultarTarefa (){
+  bloco.innerHTML = "" // limpando o elemento HTML antes de inserir novos registros, para não acumular
     const busca = query(collection(db, "tarefa"), orderBy("nome"));
 
     const resultado = await getDocs(busca);
@@ -40,13 +44,39 @@ async function consultarTarefa (){
         <strong>Status:</strong> ${item.data().status} <br>
         </div>
         <div class="d-flex gap-2 justify-content-end">
-        <button type="button" class="btn btn-danger" id="excluir">Excluir</button>
-        <button type="button" class="btn btn-info" id="alterar">Alterar</button>
+        <button type="button" class="btn btn-danger" id="${item.id}">Excluir</button>
+        <button type="button" class="btn btn-info" id="${item.id}">Alterar</button>
         </div>
     </li>
   `
+  document.querySelectorAll(".btn-danger").forEach((elemento)=>{
+    elemento.addEventListener("click",(evento)=>{
+      console.log(evento.target.id)
+      excluirTarefa(evento.target.id)
+      //alert("Botão excluir acionado")
+    })
+  })
 });
 
+document.querySelectorAll(".btn-info").forEach((elemento)=>{
+  elemento.addEventListener("click",()=>{
+    if(formAtualizar.classList.contains("d-none")){
+      formCadastrar.classList.replace("d-block", "d-none")
+      formAtualizar.classList.replace("d-none", "d-block")
+    }
+  })
+})
+
+}
+
+async function excluirTarefa(id){
+  let resultado = confirm("Tem certeza que deseja excluir?")
+  if(resultado){
+    await deleteDoc(doc(db, "tarefa", id));
+    alert("Tarefa excluida com sucesso")
+    
+    consultarTarefa() // recarregar os dados após excluir
+  }
 }
 btnTarefa.addEventListener("click", (evento)=>{
     evento.preventDefault()
@@ -54,6 +84,7 @@ btnTarefa.addEventListener("click", (evento)=>{
     inserirTarefa()
     consultarTarefa()
 })    
+
 
 
 consultarTarefa()
